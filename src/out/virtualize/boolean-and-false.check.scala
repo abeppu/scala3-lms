289be61b5109b48d89eed39a69bab37877e67e58
Aaron Beppu <abeppu@gmail.com>
2025-10-11 10:02:29 -0700

    update bool tests following Opt

--- src/out/virtualize/boolean-and-false.check.scala
+++ src/out/virtualize/boolean-and-false.check.scala
@@ -3,8 +3,7 @@ Emitting Generated Code
 *******************************************/
 class Snippet extends ((Boolean)=>(Boolean)) {
   def apply(x0:Boolean): Boolean = {
-    val x1 = x0 && false
-    x1
+    false
   }
 }
 /*****************************************
