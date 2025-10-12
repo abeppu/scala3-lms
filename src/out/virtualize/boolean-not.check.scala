632edab147d4b888ca74e2cdc21551ac04d23f63
Aaron Beppu <abeppu@gmail.com>
2025-10-11 22:46:13 -0700

    boolean test check files

---
+++ src/out/virtualize/boolean-not.check.scala
@@ -0,0 +1,12 @@
+/*****************************************
+Emitting Generated Code
+*******************************************/
+class Snippet extends ((Boolean)=>(Boolean)) {
+  def apply(x0:Boolean): Boolean = {
+    val x1 = !x0
+    x1
+  }
+}
+/*****************************************
+End of Generated Code
+*******************************************/
---
+++ src/out/virtualize/const-not.check.scala
@@ -0,0 +1,11 @@
+/*****************************************
+Emitting Generated Code
+*******************************************/
+class Snippet extends ((Boolean)=>(Boolean)) {
+  def apply(x0:Boolean): Boolean = {
+    false
+  }
+}
+/*****************************************
+End of Generated Code
+*******************************************/
